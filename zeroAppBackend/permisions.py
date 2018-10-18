8ca94a66087985153b500e50711124b96636c741
Orlando <vhagar91@gmail.com>
2018-10-18 12:29:11 -0400

    Api key permision

---
+++ zeroAppBackend/permisions.py
@@ -0,0 +1,12 @@
+from rest_framework import permissions
+from rest_framework_api_key.models import APIKey
+
+
+class OnlyAPIPermission(permissions.BasePermission):
+    message = 'Invalid or missing API Key.'
+    def has_permission(self, request, view):
+        try:
+            api_key = request.query_params.get('apikey', False)
+            return APIKey.objects.filter(id=api_key).exists()
+        except:
+            return False
