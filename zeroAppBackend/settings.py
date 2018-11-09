a5f487d94e2c8be7f65ac8edb00ae1b874bc12db
Orlando <vhagar91@gmail.com>
2018-11-09 10:19:22 -0500

    Clean up files if FileField or Image Field are updated

--- zeroAppBackend/settings.py
+++ zeroAppBackend/settings.py
@@ -14,7 +14,7 @@ import os
 from datetime import timedelta
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
