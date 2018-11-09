e2ee443e956efe5d8ebc62825627f3458804eeab
Orlando <vhagar91@gmail.com>
2018-11-09 11:20:15 -0500

    HEroku integration

--- zeroAppBackend/settings.py
+++ zeroAppBackend/settings.py
@@ -185,3 +185,10 @@ LOCALE_PATHS = (
 )
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+# Try to import django-heroku depending on Travis or Heroku
+try:
+    # Configure Django App for Heroku.
+    import django_heroku
+    django_heroku.settings(locals())
+except ImportError:
+    found = False
