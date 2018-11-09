d93a21faec51b4435134f682ad6eaccca0038590
Orlando <vhagar91@gmail.com>
2018-11-09 11:05:44 -0500

    HEroku integration

--- zeroAppBackend/settings.py
+++ zeroAppBackend/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 from datetime import timedelta
-import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -186,6 +185,3 @@ LOCALE_PATHS = (
 )
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
