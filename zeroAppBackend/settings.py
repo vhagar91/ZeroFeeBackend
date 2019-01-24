fe1b944dff5d1cd0b8e1383e01a9a7bd300ee6d4
Orlando <vhagar91@gmail.com>
2019-01-24 11:44:02 -0500

    Add Picture to listing

--- zeroAppBackend/settings.py
+++ zeroAppBackend/settings.py
@@ -149,7 +149,7 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
