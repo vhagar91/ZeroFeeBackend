cebdb39c3f6a446d2379a0e39827a2aa02eb929e
Orlando <vhagar91@gmail.com>
2019-02-26 00:31:29 +0200

    Add Picture to listing

--- zeroAppBackend/settings.py
+++ zeroAppBackend/settings.py
@@ -152,9 +152,9 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        # 'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication',
-        # 'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ),
 
 
